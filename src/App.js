93a5d92752a11e3244f106cc740bacba9b6dbb46
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-10 13:55:56 +0200

    refactory code

--- src/App.js
+++ src/App.js
@@ -1,58 +1,13 @@
-import React from "react";
-import react from 'react'
-import { useState } from 'react'
+import React, { useState } from "react";
+import LoginForm from './components/LoginForm';
 
-const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/1v/";
-const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
 
-function App() {
-  const [email, setEmail] = useState("test.user@willandskill.se")
-  const [password, setPassword] = useState("js-lesson-10")
-  const [token, setToken] = useState(null)
+function App(){
 
-  function login() {
-    const payload = {
-      email: email,
-      password:password
-    }
-    //Samma som const payload = {email, password}
-    fetch(LOGIN_URL,{
-      method: "POST",
-      headers: {
-        "content-Type": "application/json"
-      },
-      body:JSON.stringify(payload)
-    })
-    .then(res => res.json())
-    .then(data =>{
-      setToken(data.token)
-    })
-  }
   return (
     <div>
       <h1>Event app </h1>
-      <div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            name="email"
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.currentTarget.value)}
-            placeholder="john.doe@company.com"
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            name="password"
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.currentTarget.value)}
-          />
-        </div>
-        <button onClick={login}>Login</button>
-      </div>
+     <LoginForm/>
     </div>
   );
 }
